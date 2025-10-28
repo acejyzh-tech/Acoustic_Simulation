4a06680d30915698e56f6962358ad9d060f1729e
acejyzh-tech <ace.jy.zh@gmail.com>
2025-10-28 14:58:43 +0800

    Update plotting_demo.py

--- plotting_demo.py
+++ plotting_demo.py
@@ -44,7 +44,8 @@ def plotting_curve() -> None:
         This demo illustrates a combination of plotting and animation with
         Streamlit. We're generating a bunch of random numbers in a loop for around
         5 seconds. Enjoy!
-        """)
+        """
+    )
     st.pyplot(fig)
 )
 
