d5310d3ee9f5698fa4b067417a93dab7f57d0448
acejyzh-tech <ace.jy.zh@gmail.com>
2025-11-05 09:51:21 +0800

    Update plotting_demo.py

--- plotting_demo.py
+++ plotting_demo.py
@@ -51,10 +51,10 @@ def plotting_curve() -> None:
         N_total.append(mic1.N_total(f))
         
     # 绘制图形
-    df = pd.DataFrame({'freq': freqs, 'Sensitivity': sens})
+    df = pd.DataFrame({'Freq': freqs, 'Sensitivity': sens})
     chart = alt.Chart(df).mark_line().encode(
-    x=alt.X('freq', scale=alt.Scale(type='log'), title='频率（Hz）'),  # 设置横坐标为对数刻度
-    y=alt.Y('Sensitivity', title='dB')  # 纵坐标为正弦值
+    x=alt.X('Freq', scale=alt.Scale(type='log'), title='频率（Hz）'),
+    y=alt.Y('Sensitivity', title='dB')
     )
     st.altair_chart(chart)
 
