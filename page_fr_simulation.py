23cf654a9cb8394163639401d1fb641bf7472a22
acejyzh-tech <ace.jy.zh@gmail.com>
2025-11-08 18:39:56 +0800

    Update page_fr_simulation.py

--- page_fr_simulation.py
+++ page_fr_simulation.py
@@ -8,7 +8,8 @@ freq0 = ""
 for i in range(401): freq0 += str(10**(i/100+1))+"\n"
 input_data = st.text_area("请输入你想要计算的频率点数据（每行一个频率值）", freq0)
 para_data = st.text_area("请输入麦克风的参数，每行一个，格式为（编号:声孔直径（$mm$）,声孔长度（$mm$））", "MIC_0:0.3,0.2.0.15,1.3,1.85,180,280,6.0")
-
+for para in data.splitlines():
+    st.write(para)
 freqs = np.logspace(1, 5, 1000)  # 从 0.1Hz 到 100Hz
 with st.sidebar:
     st.markdown(':material/Settings: 参数设置')
