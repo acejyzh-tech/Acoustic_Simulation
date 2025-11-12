58b607db344d3ecf470b72b1e1f7c564d8cd3e84
acejyzh-tech <ace.jy.zh@gmail.com>
2025-11-12 18:42:28 +0800

    Update page_test.py

--- page_test.py
+++ page_test.py
@@ -5,5 +5,5 @@ import JYAcoustic as ac
 st.header("绘制频响及失真曲线", divider=True)
 init_fr = "35.0, 15000.0, 8.0, 25000.0, 6.0, 60000.0, 11.0\n"
 fr_para = st.text_area("请输入频响曲线参数", init_fr)
-fr_paras = [float(line.strip()) for line in fr_para.splitlines() if line.strip()]
+fr_paras = [line.strip() for line in fr_para.splitlines() if line.strip()]
 st.dataframe(fr_paras)
