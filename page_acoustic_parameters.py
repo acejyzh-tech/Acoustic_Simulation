d6fcabc033c9833d9feb7d9e6599c4a3e4327dc2
acejyzh-tech <ace.jy.zh@gmail.com>
2025-11-12 19:09:28 +0800

    Update page_acoustic_parameters.py

--- page_acoustic_parameters.py
+++ page_acoustic_parameters.py
@@ -6,11 +6,11 @@ with st.container(horizontal=True):
   temperature = st.number_input("Temperature (°C)", value=20)
   pressure = st.number_input("Pressure (Pa)", value=101325)
 data = {
-  "名称":[],
-  "英文":[],
-  "符号":[],
-  "数值":[],
-  "单位":[],
+  "名称":["玻尔兹曼常数"],
+  "英文":["Boltzmann constant"],
+  "符号":["$k_B$"],
+  "数值":[8.31446261815324 ],
+  "单位":["$J K^{-1}mol^{-1}$"],
   "量纲":[],
   "公式":[],
   "说明":[]
