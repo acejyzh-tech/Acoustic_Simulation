795c53339db5efdef7bf01333e66cc2e3590b913
acejyzh-tech <ace.jy.zh@gmail.com>
2025-11-12 19:02:33 +0800

    Update page_acoustic_parameters.py

--- page_acoustic_parameters.py
+++ page_acoustic_parameters.py
@@ -1,3 +1,14 @@
 import streamlit as st
-temperature = st.number_input("Temperature (°C)", value=20)
-pressure = st.number_input("Pressure (Pa)", value=101325)
+import pandas as pd
+
+st.header("常用声学参数", divider=True)
+with st.container(horizontal=True):
+  temperature = st.number_input("Temperature (°C)", value=20)
+  pressure = st.number_input("Pressure (Pa)", value=101325)
+data = {
+  "名称":[],
+  "符号":[]
+}
+df = pd.DataFrame(data)
+
+st.dataframe(df)
