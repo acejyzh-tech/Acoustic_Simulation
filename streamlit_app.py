bd9cce8c1ee0288278838bbd2f275a01b6255efd
acejyzh-tech <ace.jy.zh@gmail.com>
2025-11-12 12:55:30 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,7 @@ def run() -> None:
                     icon=":material/show_chart:",
                 ),
                 st.Page(
-                    dir_path / "animation_demo.py",
+                    dir_path / "page_test.py",
                     title="由低衰和谐振峰点生成麦克风频响曲线",
                     icon=":material/show_chart:",
                 ),
