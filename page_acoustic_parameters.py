8d67e65ecce09b9e21cfef331f06177a8ea9e0f0
acejyzh-tech <ace.jy.zh@gmail.com>
2025-11-13 11:27:41 +0800

    Update page_acoustic_parameters.py

--- page_acoustic_parameters.py
+++ page_acoustic_parameters.py
@@ -5,6 +5,15 @@ st.header("常用声学参数", divider=True)
 with st.container(horizontal=True):
   temperature = st.number_input("Temperature (°C)", value=20)
   pressure = st.number_input("Pressure (Pa)", value=101325)
+
+
+st.markdown(
+    """
+|  名称  | 英文 | 符号 | 数值 | 单位 | 公式 | 说明 |
+| :---- | :----| :---| :----| :---| :---| :---|
+| 玻尔兹曼常数 | Boltzmann constant | $k_B$ | 8.31446261815324 | $J K^{-1}mol^{-1}$ | - | 
+""", unsafe_allow_html=True,)
+
 data = {
   "名称":["玻尔兹曼常数"],
   "英文":["Boltzmann constant"],
