beff70af45cb1d2033ae05ced4645ce829434b66
acejyzh-tech <ace.jy.zh@gmail.com>
2025-11-22 09:37:12 +0800

    Update page_weighting.py

--- page_weighting.py
+++ page_weighting.py
@@ -7,10 +7,18 @@ freq0 = ""
 for i in range(401): freq0 += str(10**(i/100+1))+"\n"
 input_data = st.text_area("请输入你想要计算的频率点数据（每行一个频率值）", freq0)
 
-try:
-    freq = [float(line.strip()) for line in input_data.splitlines() if line.strip()]
+indata = input_data.split('\n')
+indata = [row.split(',') for row in indata]
+data = []
+for row in indata:
+    try:
+        data.append([float(num) for num in row])
+    except ValueError:
+        pass
+if len(data[0])==1:
+    freq = [row[0] for row in converted_data]
     values = [ac.A_weight(num) for num in freq]  # 计算计权值
-    
+
     df = pd.DataFrame({'频率': freq, 'A计权值': values})
     chart = alt.Chart(df).mark_line(point=True).encode(
         x=alt.X('频率:Q', title='频率 (Hz)', scale=alt.Scale(type='log')),
@@ -18,6 +26,3 @@ try:
         tooltip=['频率:Q', 'A计权值:Q']
     ).properties(title='计权值数据')
     st.altair_chart(chart, use_container_width=True)
-
-except ValueError:
-    st.error("请输入有效的数值！")
